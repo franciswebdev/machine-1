b653e4fe810391f25dec3209d3481e4c64772cbb
Frodo <francisd@sbs.com.au>
2023-09-16 11:37:42 +1000

    Converted cms to hygraph

--- src/gql/article.gql.ts
+++ src/gql/article.gql.ts
@@ -2,7 +2,8 @@ import { gql } from "graphql-request";
 
 export const ARTICLE_QUERY = gql`
   {
-    article(where: {id: "cl2wo8v3u1dc00b1fralok1er"}) {
+    article(where: {id: "clmlc7ku042pm0a194hup452z"}) {
+    # article(where: {id: "cl2wo8v3u1dc00b1fralok1er"}) {
       id
       headline
       contents {
